9cf81ffc555bbb652fb3e6a2a69ee6b18fe4aa31
Shashwat <sshashwatssingh@gmail.com>
2024-09-09 12:14:38 +0530

    Link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+
 export default function Home() {
-    return <h1>Home page</h1>
+    return (
+        <>
+            <h1>Home page</h1>
+            <Link href="/blog">Blog Page <br/></Link>
+            <Link href="/products">Products Page</Link>
+        </>
+    )
 }
