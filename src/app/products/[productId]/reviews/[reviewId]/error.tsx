33398870e465ab7227828384f5c3c71972230a1f
Shashwat <sshashwatssingh@gmail.com>
2024-09-12 13:01:10 +0530

    Granular Error Handling in Nested Routes

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-export default function ErrorBoundary({error}:{
-    error: Error}){
+export default function ErrorBoundary({
+    error,
+    reset,
+}:{
+    error: Error;
+    reset: () => void;
+}){
     return(
         <>
-            <h1>{error.message}</h1>
+            <h1>
+            {error.message}
+            <br />
+            <button onClick={reset}>Try Again</button>
+            </h1>
         </>
     )
 }
---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,9 @@
+export default function ReviewsPage(){
+    return (
+        <>
+            <h1>Here are the reviews:
+            http://localhost:3000/products/1/reviews/[reviewId]
+            </h1>
+        </>
+    );
+}
